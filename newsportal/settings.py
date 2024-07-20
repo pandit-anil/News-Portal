4d875784894207b85f393bc9e5d5734c40986ebb
pandit-anil <info.anilpandit21@gmail.com>
2024-07-20 09:00:16 +0545

    Update settings.py

--- newsportal/settings.py
+++ newsportal/settings.py
@@ -165,8 +165,8 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "info.demodjango@gmail.com"
-EMAIL_HOST_PASSWORD = "lkjo itht ppli pmjz"
+EMAIL_HOST_USER = "********"
+EMAIL_HOST_PASSWORD = "***********"
 
 
 REST_FRAMEWORK = {
